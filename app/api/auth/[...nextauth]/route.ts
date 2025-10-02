079c534fa6156648216ab9873ad134ca5aca9503
shavkat <yenediscord@gmail.com>
2025-10-02 06:48:11 +0200

    AAAAAAAAAAAAA LAIDE

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,15 @@
 export const runtime = "nodejs";
+
 import NextAuth from "next-auth";
 import Discord from "next-auth/providers/discord";
+
 const handler = NextAuth({
-  secret: process.env.NEXTAUTH_SECRET || process.env.AUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
   session: { strategy: "jwt" },
   providers: [
     Discord({
-      clientId: process.env.DISCORD_CLIENT_ID || process.env.AUTH_DISCORD_ID!,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET || process.env.AUTH_DISCORD_SECRET!,
+      clientId: process.env.DISCORD_CLIENT_ID!,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET!,
       authorization: { params: { scope: "identify email" } },
     }),
   ],
